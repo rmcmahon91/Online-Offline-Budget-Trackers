ca671caa9aba1be260c056814c626a692bf5033a
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-07-23 15:03:32 -0400

    new upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
+
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
